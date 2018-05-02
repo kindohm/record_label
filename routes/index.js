c804d40044f4cedc135c6c075dc607ced23e952c
Mike Hodnick <mike@kindohm.com>
2018-05-02 10:39:33 -0500

    added index json doc

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,23 @@ const models = require('./../data/models');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ title: 'Express' });
+  res.json({
+    artists: {
+      route: '/artists',
+      params: [
+        { name: 'pretty', type: 'boolean' }
+      ]
+    },
+    albums: {
+      route: '/albums',
+      params: [
+        { name: 'artistId', type: 'string' },
+        { name: 'page', type: 'int' },
+        { name: 'limit', type: 'int' },
+        { name: 'pretty', type: 'boolean' }
+      ]
+    }
+  });
 });
 
 module.exports = router;
